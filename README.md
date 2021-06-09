1ee84e19bca5a99a13d3df040242c7bb8bddf476
xiaowu5759 <kuaixiang789@qq.com>
2021-06-09 10:41:37 +0800

    优化pom文件和配置文件

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # ditag
 大数据bigdate，用户标签tag项目，spark，elasticsearch
+
+### 介绍
+
+### 软件架构
+
+### 依赖版本
+- 自建cdh6.3.2
+- spark2.4.0
+- scala2.12.0
+- elasticsearch7.12.0
+
+### 安装教程
+
+
+### 使用说明
+
+### 参与贡献
+
+
+
---
+++ common-java/src/main/java/com/xiaowu5759/common/manager/RedisConfig.java
@@ -0,0 +1,15 @@
+package com.xiaowu5759.common.manager;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 最好是 连接池化
+ * bean 单例化 client
+ * 然后在对象中注入
+ *
+ * @author xiaowu
+ * @date 2021/5/14 2:28 PM
+ */
+@Configuration
+public class RedisConfig {
+}
